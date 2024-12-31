5be45e37ac7c0d04799cc2c245b32b3e2b885a6c
CRUZ THE TRADER <cruzchipata5@gmail.com>
2024-12-31 09:19:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "9K8jXYhb#JqzfiIl81iwFNRoKZ0Je51Wx1SaVtGiifE6nf6OoQnk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
